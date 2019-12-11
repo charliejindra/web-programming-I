36b822d26ffd701e5263924c387983dcd7f42a0a
charliejindra <charlessjindra@gmail.com>
2019-12-11 16:19:27 -0500

    added project to github

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config.from_object(Config)
+login = LoginManager(app)
+login.login_view = 'login'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+mail = Mail(app)
+bootstrap = Bootstrap(app)
+
+from app import routes, models
